d56f94ba7476b31e8e0ca011bdbfb6c0660c2aa7
Albin Heimerson <albin.heimerson@control.lth.se>
2022-12-02 11:23:00 +0100

    restructure and add tests

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,12 +1,7 @@
 use std::{env, fs};
+use partial_sort::PartialSort;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let version = &args[1];
-
-    let input = fs::read_to_string("data/day01.txt")
-        .expect("Should have been able to read the file");
-        
+fn parse(input: &str) -> Vec<u64> {
     let mut total_carry: Vec<u64> = vec![0];
 
     input.lines().for_each(|x| {
@@ -18,12 +13,69 @@ fn main() {
         }
     });
 
+    total_carry
+}
+
+fn sum_n_largest(total_carry: &mut Vec<u64>, n: usize) -> u64 {
+    total_carry.partial_sort(n, |a, b| b.cmp(a));
+    total_carry.iter().take(n).sum::<u64>()
+}
+
+fn run_a(input: &str) -> u64 {
+    let mut total_carry = parse(input);
+    sum_n_largest(&mut total_carry, 1)
+}
+
+fn run_b(input: &str) -> u64 {
+    let mut total_carry = parse(input);
+    sum_n_largest(&mut total_carry, 3)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let version = &args[1];
+
+    let input = fs::read_to_string("data/day01.txt")
+        .expect("Should have been able to read the file");
+
     match version.as_str() {
-        "a" => println!("{}", total_carry.iter().max().unwrap()),
-        "b" => {
-            total_carry.sort_by(|a, b| b.cmp(a));
-            println!("{:?}", total_carry[0..3].iter().sum::<u64>())
-        },
+        "a" => println!("Answer: {}", run_a(&input)),
+        "b" => println!("Answer: {}", run_b(&input)),
         _ => panic!("Incorrect input, should be a or b.")
+    }
+}
+
+#[cfg(test)]
+mod day01_tests {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+        let result = run_a(input);
+        assert_eq!(result, 24000)
+    }
+
+    #[test]
+    fn test_b() {
+        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+        let result = run_b(input);
+        assert_eq!(result, 45000)
+    }
+
+    #[test]
+    fn real_a() {
+        let input = fs::read_to_string("data/day01.txt")
+            .expect("Should have been able to read the file");
+        let result = run_a(&input);
+        assert_eq!(result, 70296)
+    }
+
+    #[test]
+    fn real_b() {
+        let input = fs::read_to_string("data/day01.txt")
+            .expect("Should have been able to read the file");
+        let result = run_b(&input);
+        assert_eq!(result, 205381)
     }
 }
--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,14 +1,29 @@
 use std::{env, fs};
 
-fn score_a(opponent: i64, me: i64) -> u64 {
-    let outcome = (me - opponent) % 3;
-    let value = (me - 'X' as i64 + 1) as u64;
+fn parse(input: &str) -> impl Iterator<Item = (char, char)> + '_ {
+    input.lines()
+        .map(|line| {
+            let mut chars = line.chars();
+            let a = chars.next().unwrap();
+            chars.next();
+            let b = chars.next().unwrap();
+            (a, b)
+        })
+}
+
+fn score_a(opponent: char, me: char) -> u64 {
+    let outcome = (me as i64 - opponent as i64) % 3;
+    let value = (me as i64 - 'X' as i64 + 1) as u64;
     match outcome {
         1 => value,
         2 => 3 + value,
         0 => 6 + value,
         _ => panic!("This does not happen!")
     }
+}
+
+fn run_a(input: &str) -> u64 {
+    parse(input).map(|(a, b)| score_a(a, b)).sum()
 }
 
 fn score_b(opponent: char, result: char) -> u64 {
@@ -21,25 +36,59 @@ fn score_b(opponent: char, result: char) -> u64 {
     }
 }
 
+fn run_b(input: &str) -> u64 {
+    parse(input).map(|(a, b)| score_b(a, b)).sum()
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let version = args[1].as_str();
+    let version = &args[1];
 
     let input = fs::read_to_string("data/day02.txt")
         .expect("Should have been able to read the file");
 
-    let games = input.lines()
-        .map(|line| {
-            let mut chars = line.chars();
-            let a = chars.next().unwrap();
-            chars.next();
-            let b = chars.next().unwrap();
-            (a, b)
-        });
-        
-    match version {
-        "a" => println!("{}", games.map(|(a, b)| score_a(a as i64, b as i64)).sum::<u64>()),
-        "b" => println!("{}", games.map(|(a, b)| score_b(a, b)).sum::<u64>()),
+    match version.as_str() {
+        "a" => println!("Answer: {}", run_a(&input)),
+        "b" => println!("Answer: {}", run_b(&input)),
         _ => panic!("Incorrect input, should be a or b.")
+    }
+}
+
+#[cfg(test)]
+mod day02_tests {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        let input = "A Y\nB X\nC Z";
+        let result: Vec<u64> = parse(input).map(|(a, b)| score_a(a, b)).collect();
+
+        assert_eq!(result, vec![8, 1, 6])
+    }
+
+    #[test]
+    fn test_b() {
+        let input = "A Y\nB X\nC Z";
+        let result: Vec<u64> = parse(input).map(|(a, b)| score_b(a, b)).collect();
+
+        assert_eq!(result, vec![4, 1, 7])
+    }
+
+    #[test]
+    fn real_a() {
+        let input = fs::read_to_string("data/day02.txt")
+            .expect("Should have been able to read the file");
+        let result = run_a(&input);
+
+        assert_eq!(result, 11841)
+    }
+
+    #[test]
+    fn real_b() {
+        let input = fs::read_to_string("data/day02.txt")
+            .expect("Should have been able to read the file");
+        let result = run_b(&input);
+
+        assert_eq!(result, 13022)
     }
 }
