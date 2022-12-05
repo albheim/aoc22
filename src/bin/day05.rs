5f15fb47590343addce91b2798859dbe026d4693
Albin Heimerson <albin.heimerson@control.lth.se>
2022-12-05 02:15:20 -0800

    day05

---
+++ src/bin/day05.rs
@@ -0,0 +1,156 @@
+use std::{env, fs};
+use aoc22::split_line;
+
+struct CrateStacks {
+    stacks: Vec<Vec<char>>
+}
+
+impl CrateStacks {
+    fn from(lines: Vec<&str>) -> Self {
+        let n = (lines.last().unwrap().chars().count() + 1) / 4;
+        let mut stacks: Vec<Vec<char>> = (0..n).map(|_| Vec::new()).collect();
+
+        for line in &lines[1..] {
+            let mut chunk_iter = line.split('[');
+            let mut idx = chunk_iter.next().unwrap().chars().count() / 4;
+            for chunk in chunk_iter {
+                stacks[idx].push(chunk.chars().next().unwrap());
+                idx += (chunk.chars().count() + 1) / 4;
+            }
+        }
+
+        CrateStacks { stacks }
+    }
+
+    fn rearrange_9000(&mut self, from: usize, to: usize, n: usize) {
+        for _ in 0..n {
+            let a = self.stacks[from].pop().unwrap();
+            self.stacks[to].push(a);
+        }
+    }
+
+    fn rearrange_9001(&mut self, from: usize, to: usize, n: usize) {
+        let mut tmp = Vec::new();
+        for _ in 0..n {
+            tmp.push(self.stacks[from].pop().unwrap());
+        }
+        for _ in 0..n {
+            self.stacks[to].push(tmp.pop().unwrap());
+        }
+    }
+
+    fn toplayer(&self) -> String {
+        self.stacks.iter().map(|stack| stack.last().unwrap()).collect()
+    }
+}
+
+fn run_a(input: &str) -> String {
+    let line_iter = input.lines();
+    let mut crate_inputs: Vec<&str> = line_iter.take_while(|line| !line.is_empty()).collect();
+    crate_inputs.reverse();
+    let mut crates = CrateStacks::from(crate_inputs);
+
+    let commands = input.lines().skip_while(|line| !line.is_empty()).skip(1);
+    for line in commands {
+        let a: Vec<usize> = split_line(line, "move {} from {} to {}")
+            .unwrap()
+            .into_iter()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        crates.rearrange_9000(a[1] - 1, a[2] - 1, a[0])
+    }
+
+    crates.toplayer()
+}
+
+fn run_b(input: &str) -> String {
+    let line_iter = input.lines();
+    let mut crate_inputs: Vec<&str> = line_iter.take_while(|line| !line.is_empty()).collect();
+    crate_inputs.reverse();
+    let mut crates = CrateStacks::from(crate_inputs);
+
+    let commands = input.lines().skip_while(|line| !line.is_empty()).skip(1);
+    for line in commands {
+        let a: Vec<usize> = split_line(line, "move {} from {} to {}")
+            .unwrap()
+            .into_iter()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        crates.rearrange_9001(a[1] - 1, a[2] - 1, a[0])
+    }
+
+    crates.toplayer()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let version = &args[1];
+
+    let input = fs::read_to_string("data/day05.txt")
+        .expect("Should have been able to read the file");
+
+    match version.as_str() {
+        "a" => println!("Answer: {}", run_a(&input)),
+        "b" => println!("Answer: {}", run_b(&input)),
+        _ => panic!("Incorrect input, should be a or b.")
+    }
+}
+
+#[cfg(test)]
+mod day03_tests {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        let input = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+        let result = run_a(input);
+
+        assert_eq!(result, "CMZ");
+    }
+
+    #[test]
+    fn test_b() {
+        let input = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+        let result = run_b(input);
+
+        assert_eq!(result, "MCD");
+    }
+
+    #[test]
+    fn real_a() {
+        let input = fs::read_to_string("data/day05.txt")
+            .expect("Should have been able to read the file");
+
+        let result = run_a(&input);
+
+        assert_eq!(result, "ZBDRNPMVH");
+    }
+
+    #[test]
+    fn real_b() {
+        let input = fs::read_to_string("data/day05.txt")
+            .expect("Should have been able to read the file");
+
+        let result = run_b(&input);
+
+        assert_eq!(result, "WDLPFNNNB");
+    }
+}
