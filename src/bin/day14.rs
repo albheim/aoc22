46f6716323616e82f3b24980bf1c0398d7848c3b
Albin Heimerson <albin.heimerson@control.lth.se>
2022-12-14 16:15:27 +0100

    faster and cleaner

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -7,7 +7,7 @@ use std::fmt;
 
 const DAY: &str = "day14";
 
-#[derive(Eq, PartialEq, Hash, Clone)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
 struct Point {
     x: i64, 
     y: i64,
@@ -71,50 +71,6 @@ impl Map {
             sand_origin: Point { x: 500, y: 0 },
         }
     }
-
-    fn drop_sand(&mut self) -> bool {
-        let mut sand = self.sand_origin.clone();
-        while sand.y < self.bottom {
-            sand.y += 1;
-            if self.tiles.contains_key(&sand) {
-                sand.x -= 1;
-                if self.tiles.contains_key(&sand) {
-                    sand.x += 2;
-                    if self.tiles.contains_key(&sand) {
-                        sand.x -= 1;
-                        sand.y -= 1;
-                        self.tiles.insert(sand, Material::Sand);
-                        return true;
-                    }
-                }
-            }
-        }
-        false
-    }
-
-    fn drop_sand_floor(&mut self) -> bool {
-        let mut sand = self.sand_origin.clone();
-        while sand.y < self.bottom + 1 {
-            sand.y += 1;
-            if self.tiles.contains_key(&sand) {
-                sand.x -= 1;
-                if self.tiles.contains_key(&sand) {
-                    sand.x += 2;
-                    if self.tiles.contains_key(&sand) {
-                        sand.x -= 1;
-                        sand.y -= 1;
-                        if sand == self.sand_origin {
-                            return false
-                        }
-                        break
-                    }
-                }
-            }
-        }
-        self.tiles.insert(sand, Material::Sand);
-        true
-    }
-
 }
 
 impl fmt::Display for Map {
@@ -148,22 +104,48 @@ impl fmt::Display for Map {
     }
 }
 
+fn fill_sand<F: Fn(Point) -> bool>(map: &mut Map, done_cond: F) -> usize {
+    let mut count = 0;
+    let mut last_path = vec![map.sand_origin];
+    loop {
+        let mut sand = last_path.pop().unwrap();
+
+        while sand.y < map.bottom {
+            last_path.push(sand);
+            sand.y += 1;
+            if map.tiles.contains_key(&sand) {
+                sand.x -= 1;
+                if map.tiles.contains_key(&sand) {
+                    sand.x += 2;
+                    if map.tiles.contains_key(&sand) {
+                        sand.x -= 1;
+                        sand.y -= 1;
+                        break
+                    }
+                }
+            }
+        }
+        if done_cond(sand) {
+            return count;
+        } else {
+            count += 1;
+            last_path.pop().unwrap();
+            map.tiles.insert(sand, Material::Sand);
+        }
+    }
+}
+
 fn run_a(input: &str) -> usize {
     let mut map = Map::build(input);
-    let mut count = 0;
-    while map.drop_sand() {
-        count += 1;
-    }
-    count
+    let floor = map.bottom;
+    fill_sand(&mut map, |p: Point| p.y >= floor)
 }
 
 fn run_b(input: &str) -> usize {
     let mut map = Map::build(input);
-    let mut count = 0;
-    while map.drop_sand_floor() {
-        count += 1;
-    }
-    count + 1
+    map.bottom += 1;
+    let sand_origin = map.sand_origin;
+    fill_sand(&mut map, |p: Point| p == sand_origin) + 1
 }
 
 fn main() {
@@ -205,6 +187,6 @@ mod day08_tests {
     #[test]
     fn real_b() {
         let result = run_b(&read_data(DAY));
-        assert_eq!(result, 23111);
+        assert_eq!(result, 23416);
     }
 }
