6bcd21ee2a8245307d4e52d13278157888d5774b
Albin Heimerson <albin.heimerson@control.lth.se>
2023-01-02 15:02:06 +0100

    16 remade, still too slow

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,183 +1,261 @@
 use std::env;
 use aoc22::read_data;
 use parseline::parseln;
-use std::collections::{HashSet, HashMap};
+use std::collections::{VecDeque, HashSet, HashMap, BinaryHeap};
 
 const DAY: &str = "day16";
 
 #[derive(Debug)]
-struct Node { // Can probably make this faster by making String into usize
-    id: usize,
-    flow_rate: usize,
-    neighbours: HashSet<usize>,
-}
-
-fn to_id(s: &str) -> usize {
-    let mut a = 0;
-    for c in s.bytes() {
-        a <<= 8;
-        a |= c as usize;
-    }
-    a
-}
-
-impl Node {
-    fn parse(line: &str) -> Self {
-        parseln!(line, "Valve {} has flow rate={}; tunnel{} lead{} to valve{} {}", id: String, flow_rate: usize, _tmp: String, _tmp2: String, _tmp3: String, other: String);
-        let neighbours = other.split(", ").map(to_id).collect();
-        let n = Node {
-            id: to_id(&id),
-            flow_rate,
-            neighbours,
-        };
-        println!("Parsed {:?}", n);
-        n
-    }
-}
-
 struct Map {
-    nodes: HashMap<usize, Node>,
-    distances: HashMap<(usize, usize), usize>,
+    flow_rates: Vec<usize>,
+    distances: Vec<Vec<usize>>,
+    start_distances: Vec<usize>,
 }
 
 impl Map {
-    fn find_dist(nodes: &HashMap<usize, Node>, origin: usize, curr: usize, dist: usize, distances: &mut HashMap<(usize, usize), usize>) {
-        if let Some(&a) = distances.get(&(origin, curr)) {
-            if dist >= a {
-                return 
-            }
-        } 
-        distances.insert((origin, curr), dist);
-        for &n in nodes.get(&curr).unwrap().neighbours.iter() {
-            Map::find_dist(nodes, origin, n, dist + 1, distances);
-        }
-    }
-
     fn parse(input: &str) -> Self {
-        let nodes: HashMap<_, _> = input.lines().map(|line| {
-            let node = Node::parse(line);
-            (node.id, node)
-        }).collect();
-        let mut distances = HashMap::new();
-        for &n in nodes.keys() {
-            Map::find_dist(&nodes, n, n, 0, &mut distances);
-        }
-        Map{ nodes, distances }
-    }
+        let node_info: HashMap<String, (usize, Vec<String>)> = input.lines()
+            .map(|line| {
+                let a: Vec<&str> = line.split(' ').collect();
+                parseln!(a[4], "rate={};", rate: usize);
+                let neighbours = a[8..].iter().map(|s| s.chars().take(2).collect::<String>()).collect();
+                (String::from(a[1]), (rate, neighbours))
+            }).collect();
+        
+        let names: Vec<String> = node_info.keys().into_iter().map(String::from).collect();
 
-    fn dfs(map: &Map, state: State, path: &mut Vec<usize>) -> usize {
-        //println!("dfs {:?}", state);
-        let mut best = state.score;
-        for &n in map.nodes.keys() {
-            if !state.open.contains(&n) && map.nodes.get(&n).unwrap().flow_rate > 0 {
-                if let Some(dur) = map.distances.get(&(state.pos, n)) {
-                    let dur = dur + 1;
-                    if state.time > dur {
-                        let mut open = state.open.clone();
-                        open.insert(n);
-                        path.push(n);
-                        best = Map::dfs(map, State{ 
-                            score: state.score + (state.time - dur) * map.nodes.get(&n).unwrap().flow_rate,
-                            time: state.time - dur,
-                            pos: n,
-                            open,
-                        }, path).max(best);
-                        path.pop();
-                    }
-                }
-            }
-        }
-        //println!("score {} with time {} left and path {:?}", best, state.time, path);
-        best
-    }
+        // Floyd warshall
+        let mut distances: HashMap<(&str, &str), usize> = HashMap::new();
 
-    fn dfs2(map: &Map, state: State2, path: &mut Vec<usize>) -> usize {
-        println!("dfs {:?}", state);
-        let mut best = state.score;
-        for &n in map.nodes.keys() {
-            if !state.open.contains(&n) && map.nodes.get(&n).unwrap().flow_rate > 0 {
-                if state.time.0 < state.time.1 {
-                    if let Some(dur) = map.distances.get(&(state.pos.1, n)) {
-                        let dur = dur + 1;
-                        if state.time.1 > dur {
-                            let mut open = state.open.clone();
-                            open.insert(n);
-                            path.push(n);
-                            best = Map::dfs2(map, State2 { 
-                                score: state.score + (state.time.1 - dur) * map.nodes.get(&n).unwrap().flow_rate,
-                                time: (state.time.0, state.time.1 - dur),
-                                pos: (state.pos.0, n),
-                                open,
-                            }, path).max(best);
-                            path.pop();
-                        }
-                    }
+        for i in 0..names.len() {
+            for j in 0..names.len() {
+                if node_info.get(&names[i]).unwrap().1.contains(&names[j]) {
+                    distances.insert((&names[i], &names[j]), 1);
+                } else if i == j {
+                    distances.insert((&names[i], &names[j]), 0);
                 } else {
-                    if let Some(dur) = map.distances.get(&(state.pos.0, n)) {
-                        let dur = dur + 1;
-                        if state.time.0 > dur {
-                            let mut open = state.open.clone();
-                            open.insert(n);
-                            path.push(n);
-                            best = Map::dfs2(map, State2 { 
-                                score: state.score + (state.time.0 - dur) * map.nodes.get(&n).unwrap().flow_rate,
-                                time: (state.time.0 - dur, state.time.1),
-                                pos: (n, state.pos.1),
-                                open,
-                            }, path).max(best);
-                            path.pop();
-                        }
-                    }
+                    distances.insert((&names[i], &names[j]), 10000);
                 }
             }
         }
-        //println!("score {} with time {} left and path {:?}", best, state.time, path);
-        best
+
+        for k in 0..names.len() {
+            for i in 0..names.len() {
+                for j in 0..i {
+                    let &a = distances.get(&(&names[i], &names[k])).unwrap();
+                    let &b = distances.get(&(&names[k], &names[j])).unwrap();
+                    let &c = distances.get(&(&names[i], &names[j])).unwrap();
+                    let d = usize::min(a+b, c);
+
+                    distances.entry((&names[i], &names[j])).and_modify(|e| *e = d);
+                    distances.entry((&names[j], &names[i])).and_modify(|e| *e = d);
+                }
+            }
+        }
+
+        let filtered_names: Vec<&String> = names.iter().filter(|&name| node_info.get(name).unwrap().0 != 0).collect();
+
+
+        let flow_rates: Vec<usize> = filtered_names.iter().map(|&name| node_info.get(name).unwrap().0).collect();
+
+        let mut filtered_dists: Vec<Vec<usize>> = vec![vec![0; flow_rates.len()]; flow_rates.len()];
+        for i in  0..flow_rates.len() {
+            for j in  0..flow_rates.len() {
+                filtered_dists[i][j] = *distances.get(&(filtered_names[i], filtered_names[j])).unwrap();
+                filtered_dists[j][i] = filtered_dists[i][j];
+            }
+        }
+
+        let mut start_dists: Vec<usize> = vec![0; flow_rates.len()];
+        for i in  0..flow_rates.len() {
+            start_dists[i] = *distances.get(&(filtered_names[i], "AA")).unwrap();
+        }
+
+        Map {
+            flow_rates,
+            distances: filtered_dists,
+            start_distances: start_dists,
+        }
     }
 }
 
-#[derive(Debug)]
+fn bfs(map: &Map) -> usize {
+    let mut queue = VecDeque::new();
+    for (pos, dist) in map.start_distances.iter().enumerate() {
+        let state = State {
+            score: (30 - 1 - dist) * map.flow_rates[pos],
+            time: 30 - 1 - dist,
+            open: 1 << pos,
+            pos,
+        };
+        queue.push_back(state);
+    }
+    
+    let mut best = 0;
+    while let Some(state) = queue.pop_front() {
+        //if state.score + (0..map.distances.len()).filter(|c| ((1<<c) | state.open) == 0).map(|c| map.flow_rates[c]).sum::<usize>() * state.time < best {
+        //    continue
+        //}
+        for (newpos, &dist) in map.distances[state.pos].iter().enumerate() {
+            if state.time > dist && state.open & (1 << newpos) == 0 {
+                let newtime = state.time - 1 - dist;
+                let newstate = State {
+                    score: state.score + newtime * map.flow_rates[newpos],
+                    time: newtime,
+                    pos: newpos,
+                    open: state.open | (1 << newpos),
+                };
+                queue.push_back(newstate);
+            } else {
+                best = best.max(state.score);
+            }
+        }
+    }
+    best
+}
+
+/*
+fn bfs_heap(map: &Map) -> usize {
+    let mut visited = HashSet::new();
+    let mut queue = BinaryHeap::new();
+    for (pos, dist) in map.start_distances.iter().enumerate() {
+        let state = State {
+            score: (30 - 1 - dist) * map.flow_rates[pos],
+            time: 30 - 1 - dist,
+            open: 1 << pos,
+            pos,
+        };
+        visited.insert(state);
+        queue.push(state);
+    }
+    
+    let mut best = 0;
+    while let Some(state) = queue.pop_front() {
+        //if state.score + (0..map.distances.len()).filter(|c| ((1<<c) | state.open) == 0).map(|c| map.flow_rates[c]).sum::<usize>() * state.time < best {
+        //    continue
+        //}
+        for (newpos, &dist) in map.distances[state.pos].iter().enumerate() {
+            if state.time > dist && state.open & (1 << newpos) == 0 {
+                let newtime = state.time - 1 - dist;
+                let newstate = State {
+                    score: state.score + newtime * map.flow_rates[newpos],
+                    time: newtime,
+                    pos: newpos,
+                    open: state.open | (1 << newpos),
+                };
+                visited.insert(newstate);
+                queue.push(newstate);
+            } else {
+                best = best.max(state.score);
+            }
+        }
+    }
+    best
+}
+*/
+
+fn bfs2(map: &Map) -> usize {
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::new();
+    let total_flow: usize = map.flow_rates.iter().sum();
+    for (pos1, &dist1) in map.start_distances.iter().enumerate() {
+        for (pos2, &dist2) in map.start_distances.iter().enumerate() {
+            if pos1 != pos2 {
+                let state = if dist1 > dist2 {
+                    State2 {
+                        score: (26 - 1 - dist1) * map.flow_rates[pos1] + (26 - 1 - dist2) * map.flow_rates[pos2],
+                        time: [26 - 1 - dist2, 26 - 1 - dist1],
+                        pos: [pos2, pos1],
+                        open: (1 << pos1) | (1 << pos2),
+                        remaining_flow: total_flow - map.flow_rates[pos1] - map.flow_rates[pos2],
+                    }
+                } else {
+                    State2 {
+                        score: (26 - 1 - dist1) * map.flow_rates[pos1] + (26 - 1 - dist2) * map.flow_rates[pos2],
+                        time: [26 - 1 - dist1, 26 - 1 - dist2],
+                        pos: [pos1, pos2],
+                        open: (1 << pos1) | (1 << pos2),
+                        remaining_flow: total_flow - map.flow_rates[pos1] - map.flow_rates[pos2],
+                    }
+                };
+                queue.push_back(state);
+            }
+        }
+    }
+    
+    let mut best = 0;
+    while let Some(state) = queue.pop_front() {
+        //if state.score + (0..map.distances.len()).filter(|c| ((1<<c) | state.open) == 0).sum::<usize>() * (state.time[0] - 1) < best {
+        //    println!("State {:?} has no hope of beating best score, discarding.", state);
+        //    continue
+        //}
+        if visited.contains(&state) {
+            //println!("Already visited {:?}", state);
+            continue
+        }
+        visited.insert(state);
+        for (newpos, &dist) in map.distances[state.pos[0]].iter().enumerate() {
+            if state.time[0] > dist && state.open & (1 << newpos) == 0 {
+                let newtime = state.time[0] - 1 - dist;
+                let newstate = if newtime < state.time[1] {
+                    State2 {
+                        score: state.score + newtime * map.flow_rates[newpos],
+                        time: [state.time[1], newtime],
+                        pos: [state.pos[1], newpos],
+                        open: state.open | (1 << newpos),
+                        remaining_flow: state.remaining_flow - map.flow_rates[newpos],
+                    }
+                } else {
+                    State2 {
+                        score: state.score + newtime * map.flow_rates[newpos],
+                        time: [newtime, state.time[1]],
+                        pos: [newpos, state.pos[1]],
+                        open: state.open | (1 << newpos),
+                        remaining_flow: state.remaining_flow - map.flow_rates[newpos],
+                    }
+                };
+                queue.push_back(newstate);
+            } else {
+                best = best.max(state.score);
+            }
+        }
+
+        if state.time[1] > 3 {
+            let nomove = State2 {
+                time: [state.time[1], state.time[1] - 1],
+                ..state
+            };
+            queue.push_back(nomove);
+        }
+    }
+    best
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
 struct State {
     score: usize,
     time: usize,
     pos: usize,
-    open: HashSet<usize>,
+    open: usize,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
 struct State2 {
     score: usize,
-    time: (usize, usize),
-    pos: (usize, usize),
-    open: HashSet<usize>,
+    pos: [usize; 2],
+    time: [usize; 2],
+    open: usize,
+    remaining_flow: usize,
 }
 
 fn run_a(input: &str) -> usize {
     let map = Map::parse(input);
-
-    let state = State {
-        score: 0,
-        time: 30,
-        pos: to_id("AA"),
-        open: HashSet::new(),
-    };
-
-    let mut v = vec![];
-    Map::dfs(&map, state, &mut v)
+    bfs(&map)
 }
 
 fn run_b(input: &str) -> usize {
     let map = Map::parse(input);
-
-    let state = State2 {
-        score: 0,
-        time: (26, 26),
-        pos: (to_id("AA"), to_id("AA")),
-        open: HashSet::new(),
-    };
-
-    let mut v = vec![];
-    Map::dfs2(&map, state, &mut v)
+    bfs2(&map)
 }
 
 fn main() {
@@ -219,6 +297,6 @@ mod day16_tests {
     #[test]
     fn real_b() {
         let result = run_b(&read_data(DAY));
-        assert_eq!(result, 23416);
+        assert_eq!(result, 2292);
     }
 }
